81ac5ed160c4a56efb719967d52ea2753674d8fd
Luzie Deubel <luzie@deubels.de>
2022-06-06 20:09:24 +0200

    adding pop-up content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def about
   end
+  def one
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
+  get 'one', to: 'pages#one'
   root to: 'pages#about'
   # Generic syntax:
   # verb 'path', to: 'controller#action'
